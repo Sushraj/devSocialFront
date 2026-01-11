6a32040c397cda6acce90cb8b0b5e29e81e36ed8
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2026-01-11 11:51:29 +0530

    redux and navigation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,28 @@
 import axios from "axios";
 import { useState } from "react";
+import { addUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmaild] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://localhost:7777/login", {
-        emailId: emailId,
-        password: password,
-      });
+      const response = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(response.data));
+      return navigate("/");
     } catch (error) {
       console.error("Login failed:", error);
     }
