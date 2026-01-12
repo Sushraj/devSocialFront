f8fa6e1734cba92e11ce11eeb11bf499643c367e
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2026-01-12 17:48:21 +0530

    calling profile view api on body component load

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,29 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (error) {
+      console.log(error);
+    }
+
+    useEffect(() => {
+      fetchUser();
+    }, []);
+  };
   return (
     <div>
       <Navbar />
