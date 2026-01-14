3621ced7079e21627764d9ec395417a55ebcf14c
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2026-01-14 23:56:37 +0530

    user card

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 
 function UserCard({user}) {
     console.log(user);
-    
-  return (
-   <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src={user.photoUrl}
-      className="rounded-xl"
-      alt="user photo" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{user.firstName} {user.lastName}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-  )
+    const [firstName, lastName, photoUrl, age, gender, about] = [
+      user.firstName,
+      user.lastName,
+      user.photoUrl,
+      user.age,
+      user.gender,
+      user.about,
+    ];
+    return (
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <figure>
+          <img src={user.photoUrl} className="rounded-xl" alt="user photo" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{firstName + " " + lastName}</h2>
+          {age && gender && <p> {age + " " + gender}</p>}
+          <p>{about}</p>
+          <div className="card-actions justify-center mt-4 gap-4">
+            <button className="btn btn-primary">Ignore</button>
+            <button className="btn btn-secondary">Interested</button>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default UserCard
